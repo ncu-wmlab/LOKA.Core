baee34e3801478b0f37172643da6727ef867e01a
jcxyis@lab317-jc3 <jcxyis@g.ncu.edu.tw>
2024-03-07 21:15:52 +0800

    stargazer interaction ui

---
+++ Scripts/DemoClient/ConnectionPanel.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+
+public class ConnectionPanel : MonoBehaviour
+{
+    [SerializeField] TMP_Text _resolutionText;
+    [SerializeField] TMP_Text _localFovText;
+
+
+    [SerializeField] TMP_Text _remoteFovText;
+    [SerializeField] Slider _remoteFovSlider;
+
+    Camera _camera;
+
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        _resolutionText.text = $"{Screen.width}x{Screen.height}";
+        _localFovText.text = $"{_camera.fieldOfView:0}°";
+    }
+}
