6ab0c5ff930cb4d907f7d7c2d8e9c22b015e28bd
jcxyis@lab317-jc3 <jcxyis@g.ncu.edu.tw>
2024-03-11 23:53:14 +0800

    pc ver

---
+++ Scripts/Loka/LokaPlayer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Host player. AKA. client controlled player in the host machine.
+/// </summary>
+public class LokaPlayer : MonoBehaviour
+{
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        
+    }
+
+    void InitStream()
+    {
+        // TODO
+        
+    }
+}
