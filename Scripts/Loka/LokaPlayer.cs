42a7aec9503cf12a761170a62616d8fa734fdccf
jcxyis@lab317-jc3 <jcxyis@g.ncu.edu.tw>
2024-03-15 16:06:16 +0800

    Split stargazer package

--- Scripts/Loka/LokaPlayer.cs
+++ Scripts/Loka/LokaPlayer.cs
@@ -3,9 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.RenderStreaming;
 using UnityEngine.InputSystem;
-using UnityEngine.XR;
-using UnityEngine.InputSystem.XR;
-using UnityEngine.XR.Interaction.Toolkit;
 
 /// <summary>
 /// Host player. AKA. client controlled player in the host machine.
