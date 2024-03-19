beaa466656efad8b97fb337985f96884808c589e
jcxyis@lab317-jc3 <jcxyis@g.ncu.edu.tw>
2024-03-19 16:48:44 +0800

    feat: pico ver

---
+++ Scripts/InputDevices/InputDeviceRegister.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.XR;
+
+public class InputDeviceRegister : MonoBehaviour
+{
+    void Start()
+    {
+        InputSystem.RegisterLayout<PXR_HMD>(matches: new InputDeviceMatcher().WithInterface(XRUtilities.InterfaceMatchAnyVersion).WithProduct(@"^(PICO HMD)|^(PICO Neo)|^(PICO G)"));
+        InputSystem.RegisterLayout<PXR_Controller>(matches: new InputDeviceMatcher().WithInterface(XRUtilities.InterfaceMatchAnyVersion).WithProduct(@"^(PICO Controller)"));
+    }
+}
